eabd6c03bbac67a765cf0b288b8be16bbd1f56a2
Alex Bermúdez <alexis.gb249@gmail.com>
2025-10-15 22:13:58 -0500

    Add Card components

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,31 @@
+import './Card.css'
+
+export interface CardProps {
+  title: string,
+  description?: string,
+  imageUrl?: string,
+  linkUrl: string
+}
+
+function Card({title, description, imageUrl, linkUrl}: Readonly<CardProps>) {
+
+  return (
+    <>
+      <a href={linkUrl}>
+        <div className="card">
+          {imageUrl && (
+            <figure>
+              <img src={imageUrl} className="card-img-top" alt={title} />
+            </figure>
+          )}
+          <div className="card-body">
+            <h4>{title}</h4>
+            {description && <p className="card-text">{description}</p>}
+          </div>
+        </div>
+      </a>
+    </>
+  )
+}
+
+export default Card
