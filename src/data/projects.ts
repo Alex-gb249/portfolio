ab71fff96e4615ea9ad1908f043072d52951f7d7
Alex Bermúdez <alexis.gb249@gmail.com>
2025-10-15 22:14:23 -0500

    Add projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    title: "Pomodoro",
+    description: "A pomodoro based task manager",
+    imageUrl: "pomodoro.webp",
+    linkUrl: "https://pomodoro.axelis.dev"
+  },
+  {
+    title: "Portfolio",
+    description: "Esto parece un poco redundante, ¿no?",
+    linkUrl: "#"
+  },
+  {
+    title: "Curriculum vitae",
+    description: "Sitio donde puedes visualizar y descargar mi CV",
+    linkUrl: "https://cv.axelis.dev"
+  },
+]
+
+export default projects
